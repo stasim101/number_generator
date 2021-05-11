f96098cb677412f2c96d84dcbec980bb6d7db883
imran ansari <stasim101@gmail.com>
2021-05-11 13:55:37 +0530

    Adding batch factory to execute file in partitions

--- src/main/java/com/vmware/poc/controller/EmployeeController.java
+++ src/main/java/com/vmware/poc/controller/EmployeeController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.vmware.poc.model.Employee;
 import com.vmware.poc.service.EmployeeService;
 
--- src/main/java/com/vmware/poc/excpetion/EmployeeNotFoundException.java
+++ src/main/java/com/vmware/poc/excpetion/EmployeeNotFoundException.java
@@ -4,9 +4,11 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(NOT_FOUND)
+@ControllerAdvice
 public class EmployeeNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = -5490406453287190535L;
